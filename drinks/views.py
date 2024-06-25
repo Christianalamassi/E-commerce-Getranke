9230a765db194515551eeceb492b5e3e6bbb5c64
Ahmed Alamassi <christianalamassi@gmail.com>
2024-06-25 16:27:36 +0000

    user can sort the products

--- drinks/views.py
+++ drinks/views.py
@@ -9,21 +9,41 @@ def drinks(request):
     """ A view to show all kind of drinks, including sorting and search queries """
     
     drinks = Product.objects.all()
-    qurey = None
+    query = None
+    sort = None
+    way = None
 
     if request.GET:
         if 'search' in request.GET:
             query = request.GET['search']
             if not query:
-                message.error('Enter keyword')
+                messages.error('Enter keyword')
                 return redirect(reverse, ('drinks'))
-        queries = Q(name__icontains = query)
-        drinks = drinks.filter(queries)
+            queries = Q(name__icontains = query)
+            drinks = drinks.filter(queries)
+
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                drinks = drinks.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'descending':
+                    sortkey = f'-{sortkey}'
+            drinks = drinks.order_by(sortkey)
+
+    storing_by = f'{sort}_{way}'
+
     context = {
         'drinks': drinks,
-        'querys':query
+        'query':query,
+        'storing_by':storing_by,
     }
     return render(request, "drinks/drinks.html", context)
+
 
 def each_drink(request, drink_id):
     """ A view to show each drink's details individual """
