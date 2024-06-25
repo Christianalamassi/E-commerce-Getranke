305b91122ca063f889cd967559c66d5277c1cf01
Ahmed Alamassi <christianalamassi@gmail.com>
2024-06-25 09:41:56 +0000

    create fixtuer and added db

--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def drinks(request):
+    """ A view to show all kind of drinks, including sorting and search queries """
+
+    drinks = Product.objects.all()
+    context = {
+        'drinks': drinks
+    }
+    return render(request, "drinks/drinks.html", context)
--- mainapp/views.py
+++ mainapp/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    """ A view to the main page of the project """
+
     return render(request, "mainapp/index.html")
