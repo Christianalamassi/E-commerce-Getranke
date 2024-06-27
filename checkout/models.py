464d2e7284a7bd83e8dbaccdc18d41d2ab2bcf55
Ahmed Alamassi <christianalamassi@gmail.com>
2024-06-27 16:13:16 +0000

    Create model for checkout app

--- checkout/models.py
+++ checkout/models.py
@@ -9,20 +9,20 @@ from drinks.models import Drink
 
 class ToPay(models.Model):
 
-    state = (
-            ("London"),("Bradford"),
-            ("Wakefield"),("Nottingham"),
-            ("	Westminster"),("Coventry"),
-            ("Birmingham"),("Liverpool"),
-            ("Leeds"),("Bristol"),
-            ("Manchester"),("Leicester"),
+    states = (
+            ("London","London"),("Bradford","Bradford"),
+            ("Wakefield","Wakefield"),("Nottingham","Nottingham"),
+            ("Westminster","Westminster"),("Coventry","Coventry"),
+            ("Birmingham","Birmingham"),("Liverpool","Liverpool"),
+            ("Leeds","Leeds"),("Bristol","Bristol"),
+            ("Manchester","Manchester"),("Leicester","Leicester"),
         )
 
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=75, null=False, blank=False)
     email = models.EmailField(max_length=300, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    state =  models.CharField(choices=state, max_length=40, blank=True)
+    states =  models.CharField(choices=states, max_length=40, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -30,26 +30,20 @@ class ToPay(models.Model):
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-
+    
 
     def _generate_order_number(self):
+        """ 
+        Generate a random, unique order number using UUID 
         """
-        Generate a random, unique order number using UUID
-        """
+        
         return uuid.uuid4().hex.upper()
 
     def update_total(self):
         """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
+        Update grand total each time a line item is added
         """
         self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
-            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        else:
-            self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
     def save(self, *args, **kwargs):
@@ -66,7 +60,7 @@ class ToPay(models.Model):
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    order = models.ForeignKey(ToPay, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     drink = models.ForeignKey(Drink, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
@@ -80,4 +74,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return 
+        return f'{self.order.order_number}'
--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'mainapp',
     'drinks',
     'basket',
+    'checkout',
 ]
 
 MIDDLEWARE = [
