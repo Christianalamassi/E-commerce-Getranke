e27307c0bb95f051154d29d30228c78742a1f7b0
Ahmed Alamassi <christianalamassi@gmail.com>
2024-06-30 16:28:29 +0000

    Create webhook_hand

--- basket/contexts.py
+++ basket/contexts.py
@@ -11,28 +11,27 @@ def your_order(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, item_data in bag.items():
+    for basket_id, item_data in bag.items():
         if isinstance(item_data, int):
-            drink = get_object_or_404(Drink, pk=item_id)
+            drink = get_object_or_404(Drink, pk=basket_id)
             total += item_data * drink.price
             product_count += item_data
             bag_items.append({
-                'item_id': item_id,
+                'basket_id': basket_id,
                 'quantity': item_data,
                 'drink': drink,
             })
         else:
-            drink = get_object_or_404(Drink, pk=item_id)
+            drink = get_object_or_404(Drink, pk=basket_id)
             for size, quantity in item_data['items_by_size'].items():
                 total += quantity * drink.price
                 product_count += quantity
                 bag_items.append({
-                    'item_id': item_id,
+                    'basket_id': basket_id,
                     'quantity': quantity,
                     'drink': drink,
-                    'size': size,
                 })
-    
+    print(bag_items)
     context = {
         'bag_items': bag_items,
         'total': total,
