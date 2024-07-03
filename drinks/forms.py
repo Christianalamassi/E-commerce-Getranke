3a3f8082b67fa30a7a57d54b37f854d1f8a9b148
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-03 06:34:34 +0000

    Drink Admin and Drink form

---
+++ drinks/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Drink, Alcohol
+
+
+class DrinkForm(form.ModelForm):
+    model = Drink
+    fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        drinks = Drink.objects.all()
+        names = [(c.id, c.get_name()) for c in drinks]
+
+        self.fields['drink'].choices = names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black'
