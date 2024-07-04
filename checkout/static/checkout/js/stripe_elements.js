2edc9e3cbb7dcc824b86e455ff9982f578839b93
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-04 16:03:50 +0000

    Add change to checkout model

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -71,7 +71,7 @@ form.addEventListener('submit', function(ev) {
                 billing_details: {
                     name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
+                    // email: $.trim(form.email.value),
                     address:{
                         line2: $.trim(form.street_address.value),
                         city: $.trim(form.state.value),
