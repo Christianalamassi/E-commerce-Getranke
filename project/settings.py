4d5d391f3580f04825e876226bf64f5eef899c70
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-04 07:39:43 +0000

    Set the cloudinary in settings

--- project/settings.py
+++ project/settings.py
@@ -199,6 +199,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
