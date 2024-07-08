004c687b2e02e07884da0df60b2b8a924547c736
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-08 18:45:43 +0000

    cCreate view to newsletter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from .models import News
 from .forms import NewsForm
 
+# Create your views here.
 
 def index(request):
     """ A view to the main page of the project """
 
     return render(request, "mainapp/index.html")
+
 
 def news(request):
     """ Users newslater update"""
@@ -15,13 +17,12 @@ def news(request):
     if request.method == 'POST':
         form = NewsForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request,"You have been applied for the newsletter successfully")
-            return redirect('index')
-
+            # form.save()
+            messages.success(request,("You have been applied for the newsletter successfully."))
+        else:
+            messages.error(request, ("Somthing went wrong, try again."))
     form = NewsForm()
     context = {
         'form': form
     }
-    messages.error(request, "Somthing went wrong, try again")
     return render(request, 'mainapp/index.html', context)
