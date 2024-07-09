522b4673e0351a8eb27546d7453ef6b352935716
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-09 13:03:35 +0000

    All users and visiters can submit their question

--- contacts/views.py
+++ contacts/views.py
@@ -7,9 +7,8 @@ from .forms import QuestionForm
 def question(request):
     if request.method =='POST':
         form = QuestionForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.info(request, "We received your question, We will get back to you as soon as possible")
+        form.save()
+        messages.info(request, "We received your question, We will get back to you as soon as possible")
     form=QuestionForm()
     context={
         'form':form
