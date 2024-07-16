6bc89300690e69a60720d4a0188d39e1293e74f0
Ahmed Alamassi <christianalamassi@gmail.com>
2024-07-16 16:49:14 +0000

    Added placeholder to leave us question inputs

--- contacts/forms.py
+++ contacts/forms.py
@@ -8,3 +8,27 @@ class QuestionForm(forms.ModelForm):
         fields = [
             'name', 'email', 'message'
         ]
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+
+        super().__init__(*args, **kwargs)
+        # here is the placeholders will view in inputs
+        placeholders = {
+            'name': 'Full name',
+            'email': 'example@example.com',
+            'message': 'Enter your question here.......'
+        }
+
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]}'
+
+            # sets attributes for place holder and class and removes labels
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
